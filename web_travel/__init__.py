cfca8b55e3bc3da5f55e6aec1e25b2bb8c6db743
DenislavD <az3@abv.bg>
2026-02-18 16:33:06 +0200

    Flask app with factory base

--- web_travel/__init__.py
+++ web_travel/__init__.py
@@ -1,1 +1,27 @@
-# this file must be here
+import os
+from flask import Flask
+
+def create_app(test_config=None):
+    """Create and configure the app"""
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        # DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    app.logger.debug('A value for debugging')
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists??
+    os.makedirs(app.instance_path, exist_ok=True)
+
+    @app.route('/')
+    def index():
+        return 'Lovebirds® 2026 Dev domain on Flask 3.1.2 running on Python 3.13 !!'
+
+
+    return app
