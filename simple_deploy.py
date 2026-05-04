fac92955eb391dda7f08ac5bbc19588d0d5399da
DenislavD <az3@abv.bg>
2026-05-04 16:41:49 +0300

    Final simple deploy script TJWS-69

--- simple_deploy.py
+++ simple_deploy.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python
+import os
 import subprocess
 from functools import partial
 
-run = partial(subprocess.run, shell=True, check=True)
+REPO = '/home/aimebgl3/python/travel-dev'
+VENV_BIN = '/home/aimebgl3/virtualenv/python/travel-dev/3.13/bin'
+
+run = partial(subprocess.run, shell=True, check=True) # cwd=str
 
 def rollback():
     run('git reset --hard HEAD@{1}')
-    run('pip install -e .')
+    run(f'"{VENV_BIN}/pip" install -e .')
 
 def main():
-    run('cd /home/aimebgl3/python/travel-dev')
+    os.chdir(REPO)
     run('git pull origin dev-deni')
-    run('source /home/aimebgl3/virtualenv/python/travel-dev/3.13/bin/activate')
-    run('pip install -e .')
+    run(f'"{VENV_BIN}/pip" install -e .')
 
     try:
-        run('pytest')
+        run(f'"{VENV_BIN}/pytest"')
     except subprocess.CalledProcessError:
         rollback()
         raise SystemExit('Tests failed, reverted to previous commit.')
